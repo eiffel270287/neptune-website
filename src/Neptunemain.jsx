a7464c250946f3343c9f38db61981a27bb8ee9cb
eiffel270287 <suryadisun87@gmail.com>
2025-04-16 20:54:43 +0900

    Update and rename neptunemain.jsx to Neptunemain.jsx

---
+++ src/Neptunemain.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NeptuneMain() {
+  return (
+    <div className="text-white text-center p-10">
+      <h1 className="text-4xl font-bold mb-4">Selamat Datang di Neptune Indonesia</h1>
+      <p className="text-lg text-gray-400">Interior Design & Contractor</p>
+    </div>
+  );
+}
