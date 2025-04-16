56662b8df24243c7365b395778bcb804feaf4121
eiffel270287 <suryadisun87@gmail.com>
2025-04-16 20:49:43 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import NeptuneMain from './NeptuneMain';
+import NeptuneMain from './NeptuneMain.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
